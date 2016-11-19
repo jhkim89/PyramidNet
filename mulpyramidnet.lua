9fe4388eb54b9f95c6d2a9fef1f2ef163da13dba
Jiwhan Kim <jhkim89@kaist.ac.kr>
2016-11-19 15:03:47 +0900

    Definition of alpha is changed.

--- mulpyramidnet.lua
+++ mulpyramidnet.lua
@@ -25,7 +25,7 @@ local SBatchNorm = nn.SpatialBatchNormalization
 local function createModel(opt)
    local depth = opt.depth
    local iChannels
-   local alpha = 1.68
+   local alpha = 4.75
    local function round(x)
       return math.floor(x+0.5)
    end
@@ -61,7 +61,30 @@ local function createModel(opt)
             :add(shortcut(nInputPlane, n, stride)))
          :add(nn.CAddTable(true))
    end
+	
+   local function bottleneck(n, stride, type)
+      local nInputPlane = iChannels
+      iChannels = n * 4
 
+      local s = nn.Sequential()
+      s:add(SBatchNorm(nInputPlane))
+      s:add(Convolution(nInputPlane,n,1,1,1,1,0,0))
+      s:add(SBatchNorm(n))
+      s:add(ReLU(true))
+      s:add(Convolution(n,n,3,3,stride,stride,1,1))
+      s:add(SBatchNorm(n))
+      s:add(ReLU(true))
+      s:add(Convolution(n,n*4,1,1,1,1,0,0))
+      s:add(SBatchNorm(n*4))
+
+      return nn.Sequential()
+         :add(nn.ConcatTable()
+            :add(s)
+            :add(shortcut(nInputPlane, n * 4, stride)))
+         :add(nn.CAddTable(true))
+   end
+
+	
    -- Creates count residual blocks with specified number of features
    local function layer(block, features, count, stride)
       local s = nn.Sequential()
@@ -75,12 +98,66 @@ local function createModel(opt)
    end
 
    local model = nn.Sequential()
+   if opt.dataset == 'imagenet' then
+   -- Configurations for ResNet:
+   --  num. residual blocks, num features, residual block function
+   local cfg = {
+      [18]  = {{2, 2, 2, 2}, 512, basicblock},
+      [34]  = {{3, 4, 6, 3}, 512, basicblock},
+      [50]  = {{3, 4, 6, 3}, 2048, bottleneck},
+      [101] = {{3, 4, 23, 3}, 2048, bottleneck},
+      [152] = {{3, 8, 36, 3}, 2048, bottleneck},
+      [200] = {{3, 24, 36, 3}, 2048, bottleneck},
+   }
 
+   assert(cfg[depth], 'Invalid depth: ' .. tostring(depth))
+   local def, nFeatures, block = table.unpack(cfg[depth])
+   iChannels = 64
+   Channeltemp = 64
+   local addrate = alpha^(1/(def[1]+def[2]+def[3]+def[4]))
+   print(' | ResNet-' .. depth .. ' ImageNet')
+
+   model:add(Convolution(3,64,7,7,2,2,3,3))
+   model:add(SBatchNorm(64))
+   model:add(ReLU(true))
+   model:add(Max(3,3,2,2,1,1))
+   Channeltemp = Channeltemp * addrate
+   model:add(bottleneck(round(Channeltemp), 1, 1, 'first'))
+   for i=2,def[1] do
+      Channeltemp = Channeltemp * addrate
+      model:add(bottleneck(round(Channeltemp), 1, 1))
+   end
+   Channeltemp = Channeltemp * addrate
+   model:add(bottleneck(round(Channeltemp), 2, 1))
+   for i=2,def[2] do
+      Channeltemp = Channeltemp * addrate
+      model:add(bottleneck(round(Channeltemp), 1, 1))
+   end
+   Channeltemp = Channeltemp * addrate
+   model:add(bottleneck(round(Channeltemp), 2, 1))
+   for i=2,def[3] do
+      Channeltemp = Channeltemp * addrate
+      model:add(bottleneck(round(Channeltemp), 1, 1))
+   end
+   Channeltemp = Channeltemp * addrate
+   model:add(bottleneck(round(Channeltemp), 2, 1))
+   for i=2,def[4] do
+      Channeltemp = Channeltemp * addrate
+      model:add(bottleneck(round(Channeltemp), 1, 1))
+   end
+   model:add(nn.Copy(nil, nil, true))
+   model:add(SBatchNorm(iChannels))
+   model:add(ReLU(true))
+   model:add(Avg(7, 7, 1, 1))
+   model:add(nn.View(iChannels):setNumInputDims(3))
+   model:add(nn.Linear(iChannels, 1000))
+
+   elseif opt.dataset == 'cifar10' or opt.dataset == 'cifar100' then
    local n = (depth - 2) / 6
    iChannels = 16
    local startChannel = 16
    local Channeltemp = 16
-   addChannel = alpha^(1/n)
+   addChannel = alpha^(1/(3*n))
    print(' | PyramidNet-' .. depth .. ' CIFAR-10')
 
    model:add(Convolution(3,16,3,3,1,1,1,1))
